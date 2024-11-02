fee0d7b98e682151621f7f14b175c7802e53b758
Nennae <sezenerdal@outlook.com>
2024-11-02 23:13:32 +0100

    displayes images

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ function displayProducts(products) {
     console.log("Current product:", product);
     const productDiv = document.createElement("div");
     productDiv.innerHTML = `
+    <img src="${product.image.desktop}" alt="${product.name} thumbnail" />
       <h2>${product.name}</h2>
       <p>Category: ${product.category}</p>
       <p>Price: $${product.price.toFixed(2)}</p>
